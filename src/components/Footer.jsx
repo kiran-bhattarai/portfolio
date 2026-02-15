812991ae69f2e59ba66c6c975d1fe8d5b8fc0328
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-15 22:06:23 +0545

    feat: create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return (
+        <div className="py-2 bg-neutral-900 text-white text-center text-2xl font-mododica border-t-neutral-600 border-t">
+            Copyright &copy; {new Date().getFullYear()} Kiran Bhattarai
+        </div>
+    )
+}
+
+export default Footer
