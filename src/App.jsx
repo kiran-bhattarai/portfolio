5d273133863b1646aba40f67a528178529f0e055
Kiran Bhattarai <official.kiranbhattarai@gmail.com>
2026-02-18 22:52:03 +0545

    feat: connect routes to path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Home from "./pages/Home"
+import Default from "./pages/Default"
+import { Routes, Route } from "react-router"
+
 function App() {
 
-
   return (
-    <>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<Default />}></Route>
+      </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import ReactDOM from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
